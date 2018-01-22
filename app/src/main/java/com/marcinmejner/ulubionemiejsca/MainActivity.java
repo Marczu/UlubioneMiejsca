31b69e30ce30f2bed1d62199c9e21840e2333fb3
Marcin Mejner <marczu09@gmail.com>
2018-01-22 09:40:25 +0100

    commit 1

--- app/src/main/java/com/marcinmejner/ulubionemiejsca/MainActivity.java
+++ app/src/main/java/com/marcinmejner/ulubionemiejsca/MainActivity.java
@@ -2,13 +2,23 @@ package com.marcinmejner.ulubionemiejsca;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        final ArrayList<String> miejsca = new ArrayList<>();
+        miejsca.add("Dodaj nowe miejsce...");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, miejsca);
 
     }
 }
